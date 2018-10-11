37e435a1f17ff65eba29e0f3f732563569b8fb74
Raja <rajavn6789@gmail.com>
2018-10-11 18:26:21 +0530

    Added ret to subroutine

--- 2.hello/hello.asm
+++ 2.hello/hello.asm
@@ -1,5 +1,6 @@
 ; ----------------------------------------------------------------------------------------
 ; Writes "Hello, World" to the console using only system calls. Runs on 64-bit Linux only.
+
 ; To assemble and run:
 ; nasm -f elf64 -o hello.o hello.asm && ld hello.o -o hello
 ; ----------------------------------------------------------------------------------------
